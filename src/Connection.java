3b521e34185a7064b806f766c80317710698806e
Panov Stanislav <ju5tas@ya.ru>
2013-12-19 23:59:39 +0700

    Initial commit

--- src/Connection.java
+++ src/Connection.java
@@ -21,9 +21,9 @@ public class Connection implements Runnable {
         this.socket = socket;
         this.sender = sender;
     }
-    
+
     private PrintStream getPrintStream() throws IOException {
-        if (printStream == null){
+        if (printStream == null) {
             printStream = new PrintStream(socket.getOutputStream());
         }
         return printStream;
@@ -37,7 +37,7 @@ public class Connection implements Runnable {
     }
 
     private void unbindUnregisterClose() throws IOException {
-        System.out.println("Connection "+selfName+" closing");
+        System.out.println("Connection " + selfName + " closing");
         unbind();
         sender.unregisterConnection(this);
         socket.close();
@@ -51,29 +51,29 @@ public class Connection implements Runnable {
         return getReader().readLine();
     }
 
-    public String getName(){
+    public String getName() {
         return selfName;
     }
 
-    public boolean isConnected(){
+    public boolean isConnected() {
         return partner != null;
     }
 
-    private void setPartner(Connection partner){
+    private void setPartner(Connection partner) {
         this.partner = partner;
     }
 
-    private void bind(Connection partner){
+    private void bind(Connection partner) {
         System.out.println("Bind " + selfName + " and " + partner);
-        if (!isConnected()){
+        if (!isConnected()) {
             partner.setPartner(this);
             this.setPartner(partner);
         }
     }
 
-    public void unbind(){
-        System.out.println("Unbind "+selfName+" and "+partner);
-        if (isConnected()){
+    public void unbind() {
+        System.out.println("Unbind " + selfName + " and " + partner);
+        if (isConnected()) {
             sender.sendTo(partner.getName(), "Client interrupted the connection.");
             partner.setPartner(null);
             this.setPartner(null);
@@ -88,7 +88,7 @@ public class Connection implements Runnable {
             boolean quit = false;
             boolean skip;
 
-            while (!quit){
+            while (!quit) {
                 String message;
                 if ((message = receiveText()) == null) {
                     System.out.println("Received null text. Connection will close.");
@@ -102,7 +102,7 @@ public class Connection implements Runnable {
                 String paramStr = message.substring(0, a == -1 ? message.length() : a).toLowerCase();
 
                 skip = true;
-                switch (paramStr){
+                switch (paramStr) {
                     case "":
                         break;
                     case "/quit":
@@ -125,39 +125,17 @@ public class Connection implements Runnable {
                         skip = false;
                 }
                 if (skip) continue;
-//                if (message.isEmpty()) {
-//                    continue;
-//                }
-//                else if (message.equals("/help")){
-//                    commandHelp();
-//                    continue;
-//                }
-//                else if (message.equals("/quit")){
-//                    unbindUnregisterClose();
-//                    quit = true;
-//                    continue;
-//                }
-//                else if (message.equals("/hangup")){
-//                    commandHangUp();
-//                    continue;
-//                }
-//                else if (message.startsWith("/connect ")){
-//                    partnerName = message.substring(9).trim();
-//                    commandConnect(partnerName);
-//                    continue;
-//                }
 
-                if (isConnected()){
-                    message = "> "+message;
+                if (isConnected()) {
+                    message = "> " + message;
                     sender.sendTo(partner.getName(), message);
-                }
-                else {
+                } else {
                     message = "Unsupported command";
                     sendText(message);
                 }
             }
         } catch (Exception e) {
-            System.out.println(">>> Exception in connection "+selfName+". "+e.getMessage());
+            System.out.println(">>> Exception in connection " + selfName + ". " + e.getMessage());
             try {
                 unbindUnregisterClose();
             } catch (IOException e1) {
@@ -168,43 +146,37 @@ public class Connection implements Runnable {
     }
 
     private void commandHangUp() throws IOException {
-        if (isConnected()){
+        if (isConnected()) {
             sendText("Connection closed");
             unbind();
-        }
-        else {
+        } else {
             sendText("You must /connect before /hangup");
         }
     }
 
     private void commandConnect(String partnerName) throws IOException {
-        System.out.println("Connecting "+ selfName +" -> "+partnerName);
+        System.out.println("Connecting " + selfName + " -> " + partnerName);
         boolean reject = true;
-        if (partnerName.isEmpty()){
+        if (partnerName.isEmpty()) {
             sendText("Command /connect must have one parameter");
-        }
-        else if (isConnected()){
+        } else if (isConnected()) {
             sendText("You must /hangup before /connect");
-        }
-        else if (partnerName.equals(selfName)){
+        } else if (partnerName.equals(selfName)) {
             sendText("You can not connect with yourself");
-        }
-        else if (!sender.checkExist(partnerName)) {
-            sendText(partnerName+" was not found");
+        } else if (!sender.checkExist(partnerName)) {
+            sendText(partnerName + " was not found");
             System.out.println(partnerName + " was not found");
-        }
-        else if (sender.checkBusy(partnerName)){
-            sendText(partnerName+" is busy");
+        } else if (sender.checkBusy(partnerName)) {
+            sendText(partnerName + " is busy");
             System.out.println(partnerName + " is busy");
-        }
-        else {
+        } else {
             bind(sender.get(partnerName));
-            sendText("Connected with "+partnerName);
-            sender.sendTo(partner, "Incoming connection with "+selfName);
+            sendText("Connected with " + partnerName);
+            sender.sendTo(partner, "Incoming connection with " + selfName);
             reject = false;
         }
-        if (reject){
-            System.out.println("Reject connection "+ selfName +" -> "+partnerName+" ");
+        if (reject) {
+            System.out.println("Reject connection " + selfName + " -> " + partnerName + " ");
         }
     }
 
@@ -219,7 +191,7 @@ public class Connection implements Runnable {
     }
 
     private void commandWho() throws IOException {
-        sendText("Active connections: "+sender.activeConnections());
+        sendText("Active connections: " + sender.activeConnections());
     }
 
     @Override
